a3b008be0e5866b16c07719d815baafb7456a0c8
Daniel <komarov@arnca.pro>
2026-03-27 17:56:46 +0500

    set up main

--- custom-auth/src/main.ts
+++ custom-auth/src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = app.get(ConfigService);
+
+  app.use(cookieParser(config.getOrThrow<string>('COOKIES_SECRET')));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
+    credentials: true,
+    exposedHeaders: ['set-cookie'],
+  });
+
+  await app.listen(config.getOrThrow<number>('APPLICATION_PORT'));
 }
+
 bootstrap();
