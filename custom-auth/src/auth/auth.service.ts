c423f3a9def7bd9317258ffa2896df1763fba137
Daniel <komarov@arnca.pro>
2026-03-29 20:40:03 +0500

    create auth entity

---
+++ custom-auth/src/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+  public async register(dto: ) { }
+
+  public async login() { }
+
+  public async logout() { }
+
+  public async refresh() { }
+
+  private async saveSession() { }
+}
