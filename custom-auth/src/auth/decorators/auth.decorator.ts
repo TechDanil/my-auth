a573b8ebc70a54179ef99c1d7a5420b8f68f4ee7
Daniel <komarov@arnca.pro>
2026-03-31 22:41:41 +0500

    add auth guard decorator

---
+++ custom-auth/src/auth/decorators/auth.decorator.ts
@@ -0,0 +1,12 @@
+import { Roles } from "@/auth/decorators/roles.decorator";
+import { Role } from "@prisma/__generated__/client";
+import { applyDecorators, UseGuards } from "@nestjs/common";
+import { AuthGuard, RolesGuard } from "../guard";
+
+export const Authorization =  (...roles: Role[]) => {
+  if (roles.length > 0) {
+    return applyDecorators(Roles(...roles), UseGuards(AuthGuard, RolesGuard));
+  }
+
+  return applyDecorators(UseGuards(AuthGuard));
+};
