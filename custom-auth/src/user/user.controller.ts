cb4ff931210d299e471565e66c70b10c0ceb55bf
Daniel <komarov@arnca.pro>
2026-03-31 22:48:53 +0500

    upd user controller

--- custom-auth/src/user/user.controller.ts
+++ custom-auth/src/user/user.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, HttpCode, HttpStatus } from "@nestjs/common";
 import { UserService } from "./user.service";
+import { Authorized } from "@/auth/decorators/authorized.decorator";
+import { Authorization } from "@/auth/decorators/auth.decorator";
 
 @Controller("user")
 export class UserController {
-  constructor(private readonly userService: UserService) { }
+  #userService: UserService;
+
+  constructor(userService: UserService) {
+    this.#userService = userService;
+  }
+
+  @Authorization()
+  @HttpCode(HttpStatus.OK)
+  @Get("profile")
+  public async findProfile(@Authorized("id") userId: string) {
+    return this. #userService.findById(userId);
+  }
 }
