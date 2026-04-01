c1f4608d57f072084bfedff22a8bf23503c083ca
Daniel <komarov@arnca.pro>
2026-04-01 21:52:57 +0500

    add yandex provider

---
+++ custom-auth/src/provider/services/yandex.provider.ts
@@ -0,0 +1,47 @@
+import { BaseOauthService } from "./base-oauth.service";
+import type { TypeProviderOptions } from "./types/provider-options.type";
+import type { TypeUserInfo } from "./types/user-info.type";
+
+interface YandexProfile {
+  id: string;
+  login: string;
+  clientId: string;
+  psuid: string;
+  accessToken: string;
+  emails?: string[];
+  defaultEmail?: string;
+  isAvatarEmpty?: string;
+  defaultAvatarId?: string;
+  birthday?: string | null;
+  firstName?: string;
+  lastName?: string;
+  displayName?: string;
+  realName?: string;
+  sex?: "male" | "female" | null;
+  defaultPhone?: { id: number; number: string };
+  refreshToken?: string;
+}
+
+export class YandexProvider extends BaseOauthService {
+  constructor(options: TypeProviderOptions) {
+    super({
+      name: "yandex",
+      authorizeUrl: "https://oauth.yandex.ru/authorize",
+      accessUrl: "https://oauth.yandex.ru/token",
+      profileUrl: "https://login.yandex.ru/info?format=json",
+      scopes: options.scopes,
+      clientId: options.clientId,
+      clientSecret: options.clientSecret,
+    });
+  }
+
+  protected extractUserInfo(data: YandexProfile): TypeUserInfo {
+    return super.extractUserInfo({
+      email: data.emails[0],
+      name: data.displayName,
+      avatar: data.defaultAvatarId
+        ? `https://avatars.yandex.net/get-yapic/${data.defaultAvatarId}/islands-200`
+        : undefined,
+    });
+  }
+}
