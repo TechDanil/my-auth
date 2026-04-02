0f4c6203c47624dc7a0603e93fe278d683026aad
Daniel <komarov@arnca.pro>
2026-04-02 15:32:35 +0500

    add provider service

--- custom-auth/src/provider/provider.service.ts
+++ custom-auth/src/provider/provider.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable, OnModuleInit } from "@nestjs/common";
+import { ProvideOptionsSymbol, TypeProvideOptions } from "./provider.constants";
+import { BaseOauthService } from "./services/base-oauth.service";
 
 @Injectable()
-export class ProviderService { }
+export class ProviderService implements OnModuleInit {
+  #options: TypeProvideOptions;
+
+  constructor(@Inject(ProvideOptionsSymbol) options: TypeProvideOptions) {
+    this.#options = options;
+  }
+
+  public onModuleInit() {
+    for (const service of this.#options.services) {
+      service.baseUrl = this.#options.baseUrl;
+    }
+  }
+
+  public findByService(service: string): BaseOauthService | null {
+    return (
+      this.#options.services.find(
+        (currentService) => currentService.name === service,
+      ) ?? null
+    );
+  }
+}
