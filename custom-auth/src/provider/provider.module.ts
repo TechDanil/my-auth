83a84c0ad91ece6a89044b0a5db9de1519a4c206
Daniel <komarov@arnca.pro>
2026-04-02 15:32:51 +0500

    add provider module

--- custom-auth/src/provider/provider.module.ts
+++ custom-auth/src/provider/provider.module.ts
@@ -1,7 +1,39 @@
-import { Module } from "@nestjs/common";
+import { DynamicModule, Module } from "@nestjs/common";
+import {
+  ProvideOptionsSymbol,
+  TypeAsyncOptions,
+  TypeProvideOptions,
+} from "./provider.constants";
 import { ProviderService } from "./provider.service";
 
-@Module({
-  providers: [ProviderService],
-})
-export class ProviderModule { }
+@Module({})
+export class ProviderModule {
+  public static register(options: TypeProvideOptions): DynamicModule {
+    return {
+      module: ProviderModule,
+      providers: [
+        {
+          provide: ProvideOptionsSymbol,
+          useValue: options.services,
+        },
+        ProviderService,
+      ],
+      exports: [ProviderService],
+    };
+  }
+
+  public static registerAsync(options: TypeAsyncOptions): DynamicModule {
+    return {
+      module: ProviderModule,
+      imports: options.imports,
+      providers: [
+        {
+          provide: ProvideOptionsSymbol,
+          useFactory: options.useFactory,
+          inject: options.inject,
+        },
+      ],
+      exports: [ProviderService],
+    };
+  }
+}
