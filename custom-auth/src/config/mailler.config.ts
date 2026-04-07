99118a79fd91577a8dfe35af17447d7d784f8147
Daniel <komarov@arnca.pro>
2026-04-07 17:28:39 +0500

    set up mailer config

---
+++ custom-auth/src/config/mailler.config.ts
@@ -0,0 +1,18 @@
+import { ConfigService } from "@nestjs/config";
+import type { MailerOptions } from "@nestjs-modules/mailer";
+import { isDev } from "@/libs/utils/is-dev.util";
+
+export const getMailerConfig = (config: ConfigService): MailerOptions => ({
+  transport: {
+    host: config.getOrThrow<string>("MAIL_HOST"),
+    port: config.getOrThrow<number>("MAIL_PORT"),
+    secure: !isDev(config),
+    auth: {
+      user: config.getOrThrow<string>("MAIL_LOGIN"),
+      pass: config.getOrThrow<string>("MAIL_PASSWORD"),
+     },
+  },
+  defaults: {
+    from: `"Custom Auth" <${config.getOrThrow<string>("MAIL_LOGIN")}>`,
+  },
+});
