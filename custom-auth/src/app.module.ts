d98e7e90874728d855e11ceb112e62fed9761c3b
Daniel <komarov@arnca.pro>
2026-04-07 17:28:22 +0500

    move mail entity to a lib directory

--- custom-auth/src/app.module.ts
+++ custom-auth/src/app.module.ts
@@ -4,7 +4,7 @@ import { ConfigModule } from "@nestjs/config";
 import { PrismaModule } from "./prisma/prisma.module";
 import { AuthModule } from "./auth/auth.module";
 import { UserModule } from "./user/user.module";
-import { MailModule } from "./mail/mail.module";
+import { MailModule } from "./libs/mail/mail.module";
 
 @Module({
   imports: [
