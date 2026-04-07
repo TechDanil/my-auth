554fc2c2272e0305748cba919e37db5d8bca578b
Daniel <komarov@arnca.pro>
2026-04-07 17:18:14 +0500

    add mail entity

--- custom-auth/src/app.module.ts
+++ custom-auth/src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from "@nestjs/config";
 import { PrismaModule } from "./prisma/prisma.module";
 import { AuthModule } from "./auth/auth.module";
 import { UserModule } from "./user/user.module";
+import { MailModule } from "./mail/mail.module";
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { UserModule } from "./user/user.module";
     PrismaModule,
     AuthModule,
     UserModule,
+    MailModule,
   ],
 })
 export class AppModule { }
