440e1d930b1c3a3de7be419eaba80cb7d5607d78
Daniel <komarov@arnca.pro>
2026-04-08 16:47:46 +0500

    implement mail service

--- custom-auth/src/libs/mail/mail.service.ts
+++ custom-auth/src/libs/mail/mail.service.ts
@@ -1,4 +1,32 @@
+import { render } from "@react-email/components";
+import { MailerService } from "@nestjs-modules/mailer";
 import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+
+import { ConfirmationTemplate } from "./templates/confirmation.template";
 
 @Injectable()
-export class MailService { }
+export class MailService {
+  readonly #mailerService: MailerService;
+  readonly #configService: ConfigService;
+
+  constructor(mailerService: MailerService, configService: ConfigService) {
+    this.#mailerService = mailerService;
+    this.#configService = configService;
+  }
+
+  public async sendConfirmationEmail(email: string, token: string) {
+    const domain = this.#configService.getOrThrow<string>("ALLOWED_ORIGIN");
+    const html = await render(ConfirmationTemplate({ domain, token }));
+
+    return this.#sendMail(email, "Email Confirmation", html);
+  }
+
+  #sendMail(email: string, subject: string, html: string) {
+    return this.#mailerService.sendMail({
+      to: email,
+      subject,
+      html,
+    });
+  }
+}
