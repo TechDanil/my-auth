be51bccada21a5af89ae5e417db7fdc45364ceb0
Daniel <komarov@arnca.pro>
2026-04-23 05:43:46 +0500

    create login and sign up pages

---
+++ auth-client/src/app/auth/login/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Создать аккаунт",
+}
+
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Register</h1>
+    </div>
+  )
+} 
---
+++ auth-client/src/app/auth/register/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Создать аккаунт",
+}
+
+export default function RegisterPage() {
+  return (
+    <div>
+      <h1>Register</h1>
+    </div>
+  )
+} 
