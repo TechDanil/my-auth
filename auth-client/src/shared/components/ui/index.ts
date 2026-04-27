54fc95de22b4151b8e4c16c9cedb6097ddd82c35
Daniel <komarov@arnca.pro>
2026-04-27 16:45:43 +0500

    create Toaster component

--- auth-client/src/shared/components/ui/index.ts
+++ auth-client/src/shared/components/ui/index.ts
@@ -3,5 +3,6 @@ export { ToggleTheme } from './toggle-theme'
 export { DropdownMenu } from './dropdown-menu'
 export { Input } from './input'
 export { Label } from './label'
+export { Toaster } from './sonner'
 
 export * from './card'
