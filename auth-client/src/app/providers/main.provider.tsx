ac5cd8d2fb4f509a5da22e0279d3232ce466dc86
Daniel <komarov@arnca.pro>
2026-04-27 16:42:35 +0500

    create toast provider and use it in main provider

--- auth-client/src/app/providers/main.provider.tsx
+++ auth-client/src/app/providers/main.provider.tsx
@@ -1,6 +1,7 @@
 import { composeProviders } from './compose.provider'
 import { TanstackQueryProvider } from './tanstack-query.provider'
 import { ThemeProvider } from './theme.provider'
+import { ToastProvider } from './toast.provider'
 
 export const MainProvider = composeProviders([
   { component: TanstackQueryProvider },
@@ -12,5 +13,6 @@ export const MainProvider = composeProviders([
       disableTransitionOnChange: true,
       enableSystem: false
     }
-  }
+  },
+  { component: ToastProvider }
 ])
